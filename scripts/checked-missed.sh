195fb6ccd2040c8af062a076d52cc19a56c72b34
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2022-04-28 15:25:18 +0800

    fix: check missed use temp for result

--- scripts/checked-missed.sh
+++ scripts/checked-missed.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
 
-# jq -r "select(.language == \"$1\") | [.name, .id] | @tsv" data/repos.jsonl | sort | uniq | sort > _language.jsonl
-# jq -r '[.repository.name, .repository.id] | @tsv' "data/$2.jsonl" | sort | uniq | sort > _package.jsonl
+select=
 
-# diff _language.jsonl _package.jsonl | grep '^< ' | cut -c 3- > _diff.txt
+if [[ -n $2 ]]; then
+  select="select($2) | "
+fi
 
-rm _missed.txt
+test -d temp/ || mkdir temp
+
+pkg_mgr=$1
+f1=temp/language.txt
+f2=temp/package.txt
+f3=temp/diff.txt
+f4=temp/missed.txt
+f5=temp/missed.jsonl
+
+jq -r "${select}select(.archived == false) | select(.fork == false) | [.name, .id] | @tsv" data/repos.jsonl | sort | uniq | sort > $f1
+jq -r '[.repository.name, .repository.id] | @tsv' "data/$pkg_mgr.jsonl" | sort | uniq | sort > $f2
+
+diff $f1 $f2 | grep '^< ' | cut -c 3- > $f3
+
+test -f $f4 && rm $f4
 
 while read -r info ; do
+  test -z "$info" && break;
+
   repo_name=$(echo "$info" | awk '{print $1 " "}')
   if [[ "$repo_name" == *"-ap-"* ]] \
     || [[ "$repo_name" == *"-ap "* ]] \
     || [[ "$repo_name" == *"ehrap_"* ]] \
     || [[ "$repo_name" == *"-AP-"* ]]; then
+    echo "Ignore $repo_name"
     continue;
   fi
+
   repo_id=$(echo "$info" | awk '{print $2}')
-  gh api -i --silent "/repositories/$repo_id/contents/$2" | \
+  gh api -i --silent "/repositories/$repo_id/contents/$1" | \
     head -n 1 | \
     grep -q '404 Not Found' || \
-    echo "$info" >> _missed.txt
+    echo "$info" >> $f4
 
-  echo "finish $repo_id"
-done < _diff.txt
+  echo "Finish $repo_name"
+done < $f3
 
-template="{name: \"$2\", path: \"$2\", url: (\"https://api.github.com/repositories/\" + (.id|tostring) + \"/contents/$2\"), repository: .}"
+template="{name: \"$pkg_mgr\", path: \"$pkg_mgr\", url: (\"https://api.github.com/repositories/\" + (.id|tostring) + \"/contents/$pkg_mgr\"), repository: .}"
 
-awk '{print $1}' _missed.txt \
+awk '{print $1}' $f4 \
   | xargs -I'_' jq -c "select(.name == \"_\") | $template" data/repos.jsonl \
-  > _missed.jsonl
+  > $f5
 
-cat "data/$2.jsonl" _missed.jsonl > "data/$2-merged.jsonl"
+cat "data/$pkg_mgr.jsonl" $f5 > "data/$pkg_mgr-merged.jsonl"
